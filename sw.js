b3c9861401fd3558a2d0a83cef9c66bf47e3cb0f
leomardagoatlolg <leomardagoatlol@gmail.com>
2025-12-03 17:08:01 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  const options = {
+    body: data.body,
+    icon: '/icon.png', // Add a 192x192 PNG in public/
+    badge: '/badge.png',
+    data: { url: data.url }
+  };
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url));
+});
